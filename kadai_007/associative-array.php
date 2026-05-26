19c5fe6df696bb528d2ba69974cb6987e2e6bce1
k-for-learn <wG3qaKc.tSTcz3sD@gmail.com>
2026-05-26 23:32:46 +0900

    kadai_007 first commit

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP課題007</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        $sample=['name'=>'onion','price'=>200,'weight'=>160];
+        print_r($sample);
+        ?>
+    </p>
+</body>
+
+</html>
