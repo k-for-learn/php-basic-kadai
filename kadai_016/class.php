f85f84a061dfde22e34c839a1233308f3c364427
k-for-learn <wG3qaKc.tSTcz3sD@gmail.com>
2026-05-28 21:28:27 +0900

    kadai_016 first commit

---
+++ kadai_016/class.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        // クラスを定義する
+        class Food
+        {
+            private $name;
+            private $price;
+
+            public function __construct(string $name, int $price)
+            {
+                $this->name = $name;
+                $this->price = $price;
+            }
+
+            public function show_price()
+            {
+                echo $this->price . '<br>';
+            }
+        }
+
+        class Animal
+        {
+            private $name;
+            private $height;
+            private $weight;
+
+            public function __construct(string $name, int $height, int $weight)
+            {
+                $this->name = $name;
+                $this->height = $height;
+                $this->weight = $weight;
+            }
+
+            function show_height()
+            {
+                echo $this->height . '<br>';
+            }
+        }
+
+        $food = new Food('potate', 250);
+        print_r($food);
+        echo '<br>';
+
+        $animal = new Animal('dog', 60, 5000);
+        print_r($animal);
+        echo '<br>';
+
+        $food->show_price();
+        $animal->show_height();
+        ?>
+    </p>
+</body>
+
+</html>
